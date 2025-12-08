e5387c77c063ce5d3b6621cf5659c2baca879b0f
Drzxn <darshanr1290@gmail.com>
2025-12-08 22:14:39 +0530

    Test 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.dagger.hilt.android")
-    kotlin("kapt")
-    id("com.google.gms.google-services")
+    id("com.android.application") version "8.13.1"
+    id("org.jetbrains.kotlin.android") version "1.9.21"
+    id("com.google.dagger.hilt.android") version "2.48"
+    kotlin("kapt") version "1.9.21"
+    id("com.google.gms.google-services") version "4.4.1"
 }
 
 android {
@@ -18,6 +18,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -32,27 +33,32 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         compose = true
         buildConfig = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-    aaptOptions {
-        noCompress("tflite")
+    androidResources {
+        noCompress += listOf("tflite")
     }
 }
 
@@ -61,6 +67,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+
+    // Compose
     implementation("androidx.compose.ui:ui:1.6.0")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.0")
     implementation("androidx.compose.foundation:foundation:1.6.0")
@@ -80,7 +88,7 @@ dependencies {
     // Machine Learning
     implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.3")
     implementation("org.tensorflow:tensorflow-lite-gpu-delegate-plugin:0.4.3")
-    implementation("org.tensorflow:tensorflow-lite-gpu:2.9.0") // Or a more recent stable version
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.9.0")
     implementation("com.google.mlkit:object-detection:17.0.1")
 
     // Dependency Injection (Hilt)
@@ -108,6 +116,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
@@ -115,3 +124,4 @@ dependencies {
 kapt {
     correctErrorTypes = true
 }
+
